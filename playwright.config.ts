6d66cea4f0883b5753ee3a794e16a502e997f4c4
Yasmeen-Fathima <yashiforwork@gmail.com>
2026-06-02 11:57:50 +1000

    added PR comments

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [['html', { open: 'never' }], ['list']],
+  reporter: [['html', { open: 'never' }], ['list'], ['json', {outputFile: 'test-resourceLimits.json'}]],
   timeout: process.env.CI ? 120000 : 60000,
 
   use: {
